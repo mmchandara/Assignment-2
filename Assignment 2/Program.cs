e63d2e744908aa60a4f7950f782ceeab8cc0f50a
infin <infin@AKA>
2024-01-28 17:53:23 -0600

    Add project files.

---
+++ Assignment 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input numbers: ");
+            string input = Console.ReadLine();
+            try
+            {
+                char[] inputArray = input.ToCharArray();
+
+                int[] num = Array.ConvertAll(inputArray, i => int.Parse(i.ToString()));
+
+                Console.WriteLine("Elements in Array: ");
+                foreach (int i in num)
+                {
+                    Console.WriteLine(i);
+                }
+
+                Console.WriteLine("Reversed Array: ");
+                Array.Reverse(num);
+                Array.ForEach<int>(num, i => Console.WriteLine(i));
+
+                Console.WriteLine($"The sum is: {num.Sum()}");
+            }
+            catch (Exception e)
+            {
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
